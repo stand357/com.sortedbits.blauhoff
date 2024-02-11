4262392b294227f7e773eb3b12039b7d6eb01bc8
Wim Haanstra <wim@x256.nl>
2024-02-11 10:25:45 +0100

    Many register changes

--- drivers/blauhoff-modbus/devices/blauhoff/spha.ts
+++ drivers/blauhoff-modbus/devices/blauhoff/spha.ts
@@ -14,7 +14,6 @@ import { ModbusDeviceDefinition } from '../../../../api/modbus/models/modbus-dev
  * Field 4: Scale
  * Field 5: Capability ID
  * Field 6: Calculation that needs to be performed on the value
- * Field 7: Multiplier
  */
 const inputRegisters = [
     new ModbusRegister(36101, 1, RegisterDataType.UINT16, 1, 'status_code.run_mode', RegisterCalculation.None),
@@ -30,17 +29,29 @@ const inputRegisters = [
 
     new ModbusRegister(36112, 1, RegisterDataType.UINT16, 1, 'measure_power.pv1', RegisterCalculation.None),
     new ModbusRegister(36113, 1, RegisterDataType.UINT16, 1, 'measure_power.pv2', RegisterCalculation.None),
-    new ModbusRegister(36205, 2, RegisterDataType.INT32, 1, 'measure_power.pv', RegisterCalculation.None),
+
+    new ModbusRegister(36117, 2, RegisterDataType.INT32, 1, 'measure_power.dsp', RegisterCalculation.None),
+    new ModbusRegister(36124, 2, RegisterDataType.INT32, 1, 'measure_power.eps', RegisterCalculation.None),
+    new ModbusRegister(36131, 2, RegisterDataType.INT32, 1, 'measure_power.grid_output', RegisterCalculation.None),
+    new ModbusRegister(36138, 2, RegisterDataType.INT32, 1, 'measure_power.battery', RegisterCalculation.None),
+
+    new ModbusRegister(36151, 1, RegisterDataType.UINT16, 0.1, 'measure_voltage.battery', RegisterCalculation.None),
 
     new ModbusRegister(36155, 1, RegisterDataType.UINT16, 1, 'measure_state_of_charge.bat_soc', RegisterCalculation.None),
     new ModbusRegister(36156, 1, RegisterDataType.UINT16, 1, 'measure_state_of_charge.bat_soh', RegisterCalculation.None),
 
-    new ModbusRegister(36151, 1, RegisterDataType.UINT16, 0.1, 'measure_voltage.battery', RegisterCalculation.None),
-
     new ModbusRegister(36161, 1, RegisterDataType.UINT16, 0.1, 'measure_temperature.battery_high', RegisterCalculation.None),
     new ModbusRegister(36163, 1, RegisterDataType.UINT16, 0.1, 'measure_temperature.battery_low', RegisterCalculation.None),
-    new ModbusRegister(36117, 2, RegisterDataType.INT32, 0.1, 'measure_power.ac', RegisterCalculation.None),
 
+    new ModbusRegister(36201, 2, RegisterDataType.UINT32, 0.1, 'meter_power.battery_total_charge', RegisterCalculation.None),
+    new ModbusRegister(36203, 2, RegisterDataType.UINT32, 0.1, 'meter_power.battery_total_discharge', RegisterCalculation.None),
+
+    new ModbusRegister(36205, 2, RegisterDataType.INT32, 1, 'measure_power.pv', RegisterCalculation.None),
+
+    new ModbusRegister(36207, 2, RegisterDataType.UINT32, 0.1, 'meter_power.total_to_grid_pv', RegisterCalculation.None),
+    new ModbusRegister(36209, 2, RegisterDataType.UINT32, 0.1, 'meter_power.total_to_grid', RegisterCalculation.None),
+    new ModbusRegister(36211, 2, RegisterDataType.UINT32, 0.1, 'meter_power.total_from_grid', RegisterCalculation.None),
+    new ModbusRegister(36213, 2, RegisterDataType.UINT32, 0.1, 'meter_power.pv', RegisterCalculation.None),
 ];
 
 const holdingRegisters: ModbusRegister[] = [
