bead3eec0883420e50cf08206a3236259b0fd5c5
Wim Haanstra <wim@x256.nl>
2024-02-12 06:38:10 +0100

    Update documentation

---
+++ docs/README-template.md
@@ -0,0 +1,27 @@
+# BlauHoff
+
+Adds support for BlauHoff devices
+
+![Workflow](https://github.com/sorted-bits/com.sortedbits.blauhoff/actions/workflows/node.js.yml/badge.svg)
+
+## Authentication
+For this Homey app to work, you need to have a valid `Access ID` and `Access Secret`. Those can be aquired by visiting [this URL](https://sortedbits.com).
+
+## Registering your devices serial
+For this app to work, you also need to bind your device to your account. During the pairing process in Homey, there is a field to enter your serial. If you specify your serial there, the device will be bound to your account.
+
+## Flows
+### Actions
+The following actions are available in Homey flows:
+
+- Set self consumption mode (VPP mode1)
+- Set direct charge mode (VPP mode2)
+- Set direct discharge mode (VPP mode3)
+- Charge only，no discharging (VPP mode 4)
+- Discharge only to the load, avoid charging (VPP mode 5)
+- Inverter outputs at specified power (VPP mode 6)
+- Inverter operates at the specified power (VPP mode 7)
+
+# Modbus Register information
+
+{{MODBUS_REGISTERS}}
---
+++ scripts/modbus/document-registers.ts
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import { devices } from '../../drivers/blauhoff-modbus/devices/devices';
+import { orderModbusRegisters } from '../../drivers/blauhoff-modbus/helpers/order-modbus-registers';
+import { brands } from '../../drivers/blauhoff-modbus/models/brand';
+
+let output = '';
+
+brands.forEach((brand) => {
+    const models = devices.filter((device) => device.brand === brand);
+
+    output += `# ${brand.toLocaleUpperCase()}\n`;
+    models.forEach((model) => {
+        output += `## ${model.name}\n`;
+        output += `${model.description}\n\n`;
+
+        const registers = model.getDefinition();
+
+        output += '### Input Registers\n';
+        output += '| Address | Length | Data Type | Scale | Capability ID | Calculation |\n';
+        output += '| ------- | ------ | --------- | ----- | ------------- | ----------- |\n';
+        orderModbusRegisters(registers.inputRegisters).forEach((register) => {
+            output += `| ${register.address} | ${register.length} | ${register.dataType.toString()} | ${register.scale} | ${register.capabilityId} | ${register.calculation} |\n`;
+        });
+
+        output += '\n### Holding Registers\n';
+        output += '| Address | Length | Data Type | Scale | Capability ID | Calculation |\n';
+        output += '| ------- | ------ | --------- | ----- | ------------- | ----------- |\n';
+        orderModbusRegisters(registers.holdingRegisters).forEach((register) => {
+            output += `| ${register.address} | ${register.length} | ${register.dataType.toString()} | ${register.scale} | ${register.capabilityId} | ${register.calculation} |\n`;
+        });
+
+        output += '\n';
+    });
+});
+
+fs.writeFileSync('../../.build/modbus-registers.md', output);
+
+const readme = fs.readFileSync('../../docs/README-template.md', 'utf8');
+
+fs.writeFileSync('../../README.md', readme.replace('{{MODBUS_REGISTERS}}', output));
