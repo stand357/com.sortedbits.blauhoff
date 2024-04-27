1a95779c19b24578a3b2fd368b6a0e31a5708624
Wim Haanstra <wim@x256.nl>
2024-04-27 09:51:19 +0200

    Allow time of use settings for Deye

--- api/modbus/models/device-model.ts
+++ api/modbus/models/device-model.ts
@@ -19,6 +19,9 @@ export enum SupportedFlowTypes {
     set_grid_peak_shaving_on = 'set_grid_peak_shaving_on',
     set_grid_peak_shaving_off = 'set_grid_peak_shaving_off',
     set_work_mode_and_zero_export_power = 'set_work_mode_and_zero_export_power',
+    set_time_of_use_enabled = 'set_time_of_use_enabled',
+    set_time_of_use_day_enabled = 'set_time_of_use_day_enabled',
+    set_time_of_use_timeslot_parameters = 'set_time_of_use_timeslot_parameters',
 }
 
 export const getSupportedFlowTypes = (): string[] => {
